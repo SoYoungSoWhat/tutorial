d001b5325936efc590b8f18f99d57dde59d8d29a
SoYoung <sy2766@gmail.com>
2016-01-10 13:58:32 +0800

    test for commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	vector<int> V(10);
+	map<string, int> M;
+	M["a"] = 1;
+	M["ba"] = 2;
+	return 0;
+}
